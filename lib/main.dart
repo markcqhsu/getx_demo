701df61b05d877fbad7d346aec409788b8e03d1e
Mark <kkboytest@gmail.com>
2021-08-27 13:49:16 +0800

    getx 計數器

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:gtex_demo/controller.dart';
 
 void main() {
   runApp(GetMaterialApp(home: Home()));
@@ -10,6 +11,7 @@ class Home extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final controller = Get.put(Controller());
     return Scaffold(
       appBar: AppBar(
         title: Text("GetX Demo"),
@@ -18,23 +20,50 @@ class Home extends StatelessWidget {
       body: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
-          Container(
-              height: 200,
-              width: 200,
-              color: Colors.red,
-              child: Align(
-                alignment: Alignment.bottomCenter,
-                child: Text(
-                  "1111",
-                  style: TextStyle(fontSize: 50, color: Colors.white),
-                ),
-              ))
+          GestureDetector(
+            onTap: () {
+              Get.to(Other());
+            },
+            child: Container(
+                height: 200,
+                width: 200,
+                color: Colors.red,
+                child: Align(
+                  alignment: Alignment.bottomCenter,
+                  child: Obx(() => Text(
+                        controller.count.toString(),
+                        style: TextStyle(fontSize: 50, color: Colors.white),
+                      )),
+                )),
+          )
         ],
       ),
       floatingActionButton: FloatingActionButton(
         child: Icon(Icons.add),
-        onPressed: () {},
+        onPressed: () {
+          controller.increment();
+        },
       ),
     );
+  }
+}
+
+class Other extends StatelessWidget {
+  // 你可以让Get找到一个正在被其他页面使用的Controller，并将它返回给你。
+  final Controller c = Get.find();
+
+  @override
+  Widget build(context) {
+    // 访问更新后的计数变量
+    return Scaffold(
+        appBar: AppBar(
+          leading: GestureDetector(
+            onTap: () {
+              Get.back();
+            },
+            child: Icon(Icons.arrow_back),
+          ),
+        ),
+        body: Center(child: Text("${c.count}")));
   }
 }
