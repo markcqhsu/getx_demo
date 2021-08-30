57b86b7a62505b7a15c29d2ec1e67fce12263f76
Mark <kkboy.iphone@gmail.com>
2021-08-30 22:28:34 +0800

    Getx_Workers

--- lib/controller.dart
+++ lib/controller.dart
@@ -1,4 +1,5 @@
 import 'package:get/get.dart';
+import 'package:get/get_rx/src/rx_typedefs/rx_typedefs.dart';
 
 class Controller extends GetxController {
   // var count = 1.obs;
@@ -6,4 +7,24 @@ class Controller extends GetxController {
   final count1 = 0.obs;
   final count2 = 0.obs;
   int get sum => count1.value + count2.value;
+
+  @override
+  void onInit() {
+    // TODO: implement onInit
+    super.onInit();
+      
+    //* 可以用來計算購物車價格, 購買數量等等
+    // ever(count1, (callback) {
+    //   print(callback);
+    // });
+    //* 可以用來保存用戶訊息
+    // once(count1, (callback) {
+    //   print(callback.toString() + "_once demo");
+    // });
+
+    //搜索場景
+    debounce(count1, (callback) {
+      print(callback.toString() + "_debounce demo");
+    }, time: Duration(seconds: 3));
+  }
 }
